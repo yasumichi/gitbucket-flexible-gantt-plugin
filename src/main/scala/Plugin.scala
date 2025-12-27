498544a28930190484eb86b1d1e6311d641b06c3
Yasumichi Akahoshi <sekiseijuju@gmail.com>
2025-12-27 22:38:55 +0900

    update version to 0.1.1

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -24,7 +24,8 @@ class Plugin extends gitbucket.core.plugin.Plugin with AccountService with Repos
   override val pluginName: String = "Flexible Gantt Plugin"
   override val description: String = "Flexible Gantt Plugin"
   override val versions: List[Version] = List(
-    new Version("0.1.0", new LiquibaseMigration("update/gitbucket-flexible-gantt-0.1.0.xml"))
+    new Version("0.1.0", new LiquibaseMigration("update/gitbucket-flexible-gantt-0.1.0.xml")),
+    new Version("0.1.1")
   )
 
   override val controllers = Seq(
