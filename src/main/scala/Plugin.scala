243490f2a6de4af74fa1369031fb6c0e357ec641
Yasumichi Akahoshi <sekiseijuju@gmail.com>
2026-01-09 22:29:47 +0900

    update version to 0.5.3

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -45,7 +45,8 @@ class Plugin extends gitbucket.core.plugin.Plugin with AccountService with Repos
     new Version("0.4.2"),
     new Version("0.5.0"),
     new Version("0.5.1"),
-    new Version("0.5.2")
+    new Version("0.5.2"),
+    new Version("0.5.3")
   )
 
   /**
